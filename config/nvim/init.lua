821b8580823b665fb9b09c4cf77741510f29473f
Lawliet529 <85656044+Lawliet529@users.noreply.github.com>
2022-11-30 20:05:44 +0700

    Add neovim config

---
+++ config/nvim/init.lua
@@ -0,0 +1,31 @@
+-- Set line numbers
+vim.opt.number = true
+-- Highlight current line
+vim.opt.cursorline = true
+-- Use spaces instead of tabs
+vim.opt.expandtab = true
+-- Set tab width to 2 spaces
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+
+if require('plugins') then return end
+
+-- Set colorscheme
+vim.opt.termguicolors = true
+vim.g.nord_cursor_line_number_background = 1
+vim.g.nord_uniform_diff_background = 1
+vim.cmd('colorscheme nord')
+
+-- Set statusline
+require('lualine').setup()
+vim.opt.showmode = false -- Hide mode
+
+-- nvim-treesitter
+require('nvim-treesitter.configs').setup {
+  ensure_installed = { "lua", "vim" },
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+}
---
+++ config/nvim/lua/plugins.lua
@@ -0,0 +1,53 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    print('Cloning packer.nvim...')
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+require('packer').startup(function(use)
+  -- Manage packer itself
+  use 'wbthomason/packer.nvim'
+
+  -- Nord theme
+  use 'arcticicestudio/nord-vim'
+
+  -- Github Copilot
+  use 'github/copilot.vim'
+
+  -- nvim-tree & nvim-web-devicons
+  use 'nvim-tree/nvim-web-devicons'
+
+  -- lualine: statusline
+  use 'nvim-lualine/lualine.nvim'
+
+  -- treesitter: syntax highlighting
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end,
+  }
+
+  -- markdown-preview
+  use({
+    "iamcco/markdown-preview.nvim",
+    run = function() vim.fn["mkdp#util#install"]() end,
+  })
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
+
+return packer_bootstrap
