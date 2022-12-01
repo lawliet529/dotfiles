7b62866866699270030de0ed5aa360a4c4892769
Lawliet529 <85656044+Lawliet529@users.noreply.github.com>
2022-12-02 01:03:22 +0700

    Add bashrc.d

---
+++ bashrc.d/aliases.sh
@@ -0,0 +1,3 @@
+alias mountdrive="rclone mount drive: rclone/Google\ Drive/ --vfs-cache-mode full --daemon"
+alias mountdocs="rclone mount docs: rclone/Documents/ --vfs-cache-mode full --daemon"
+alias mpv="flatpak run io.mpv.Mpv"
