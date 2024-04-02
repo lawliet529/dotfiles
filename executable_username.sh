978433806421947493a5e999fb5886f22adef8fc
lawliet529 <85656044+lawliet529@users.noreply.github.com>
2024-04-02 09:36:57 +0700

    Reorganize files

--- executable_username.sh
+++ executable_username.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-wget -q -nc -O /tmp/eff_large_wordlist.txt https://github.com/lawliet529/dotfiles/raw/main/local/share/dict/eff_large_wordlist.txt
+wget -q -nc -O /tmp/eff_large_wordlist.txt https://github.com/lawliet529/dotfiles/raw/main/eff_large_wordlist.txt
 
 echo $(shuf -n1 /tmp/eff_large_wordlist.txt)$(cat /dev/urandom | tr -cd '0-9' | fold -w4 | head -n1)
