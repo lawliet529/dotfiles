462862cc1dfc4631196c62abeb58849e8f947878
lawliet529 <85656044+lawliet529@users.noreply.github.com>
2024-04-04 13:25:12 +0700

    Add gnome.sh

---
+++ executable_gnome.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Sort applications by name
+gsettings set org.gnome.shell app-picker-layout "[]"
+
+# Disable gestures for XWayland apps (wine apps crashes on multitouch gestures)
+xinput list --name-only | grep ^xwayland-pointer-gestures | xargs -n1 xinput disable
