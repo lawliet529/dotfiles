00cd4f01d946094c0c2623ce73b3d0b01d1eb2f7
lawliet529 <85656044+lawliet529@users.noreply.github.com>
2024-04-07 12:48:29 +0700

    Fix typo

--- executable_gnome.sh
+++ executable_gnome.sh
@@ -3,5 +3,5 @@
 # Sort applications by name
 gsettings set org.gnome.shell app-picker-layout "[]"
 
-# Disable gestures for XWayland apps (wine apps crashes on multitouch gestures)
+# Disable gestures for XWayland apps (wine apps crash on multitouch gestures)
 xinput list --name-only | grep ^xwayland-pointer-gestures | xargs -n1 xinput disable
