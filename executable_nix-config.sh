df4cc025fc8a96fedb043f87eab44b6a19b10b3d
lawliet529 <85656044+lawliet529@users.noreply.github.com>
2024-04-17 16:14:27 +0700

    Add nix-config.sh

---
+++ executable_nix-config.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+cp -r /etc/nixos/ /tmp/
+$EDITOR /tmp/nixos/configuration.nix
+if ! command -v nixfmt &> /dev/null
+then
+    echo "nixfmt could not be found. Install nixfmt to format your config."
+else
+  nixfmt /tmp/nixos/configuration.nix &>/dev/null \
+    || ( nixfmt /tmp/nixos/configuration.nix; echo "Reformatting failed!" && exit 1)
+fi
+
+if cmp -s /etc/nixos/configuration.nix /tmp/nixos/configuration.nix; then
+  echo "No changes. Exiting..."
+else
+  echo "Changes detected."
+  diff -U0 /etc/nixos/configuration.nix /tmp/nixos/configuration.nix
+  cp /etc/nixos/configuration.nix /tmp/nixos/configuration.$(date +%s)$$.nix
+  echo "Writing to /etc/nixos/configuration.nix..."
+  sudo cp /tmp/nixos/configuration.nix /etc/nixos/configuration.nix && echo "New config written to /etc/nixos/configuration.nix" \
+  && read -p "Run nixos-rebuild? (Y/n): " confirm \
+  && if [[ $confirm == "" || $confirm == [yY] ]]; then
+    sudo nixos-rebuild switch
+  fi
+fi
