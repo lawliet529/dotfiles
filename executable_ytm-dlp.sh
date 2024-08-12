dcae6026a73ca50a0ac17ef85ceafcef3994168b
lawliet529 <85656044+lawliet529@users.noreply.github.com>
2024-08-12 14:52:30 +0700

    Update ytm-dlp.sh

--- executable_ytm-dlp.sh
+++ executable_ytm-dlp.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-yt-dlp -f ba --remux-video ogg --embed-metadata -P /tmp/ytm-dlp -o "%(playlist)s/%(playlist_index)s %(title)s.%(ext)s" $@
-
-beet import /tmp/ytm-dlp
+yt-dlp -f ba --remux-video ogg --embed-metadata -o "%(playlist)s/%(playlist_index)s %(title)s.%(ext)s" $@
