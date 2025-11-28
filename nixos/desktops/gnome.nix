43239c93153e07fb64f7706e6ee53bef93834ef6
lawliet529 <85656044+lawliet529@users.noreply.github.com>
2025-11-28 17:13:06 +0700

    Add nixos

---
+++ nixos/desktops/gnome.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, ... }:
+{
+  i18n.inputMethod = {
+    enabled = "ibus";
+    ibus.engines = with pkgs.ibus-engines; [ bamboo ];
+  };
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.enable = true;
+  services.xserver.excludePackages = [ pkgs.xterm ];
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  environment.gnome.excludePackages =
+    (with pkgs; [
+      gnome-tour
+      gnome-software
+      gnome-music
+      epiphany # web browser
+      geary # email reader
+      totem # video player
+    ])
+
+    ++ (with pkgs.gnome; [
+    ]);
+
+  environment.systemPackages = with pkgs; [
+    adw-gtk3
+    gnome-tweaks
+    zenity
+    (papirus-icon-theme.override { color = "adwaita"; })
+  ];
+}
